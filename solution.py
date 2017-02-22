7e424e1c24dae25a71d8f02a8a949bfbc2e02e8b
Bhavin Patel <bpatel@Bhavins-MBP.fios-router.home>
2017-02-22 01:47:59 -0500

    worked on solution for diagonal sudoku

--- solution.py
+++ solution.py
@@ -1,5 +1,19 @@
 assignments = []
 
+from pprint import  pprint
+import  sys
+import itertools
+
+digits = '123456789'
+alphas = 'ABCDEFGHI'
+
+cols = {}
+rows = {}
+boxes = {}
+diagonals = {}
+cell_constraints = {}
+
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -24,6 +38,7 @@ def naked_twins(values):
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
+    return [ "{}{}".format(x,y) for x,y in itertools.product(A,B) ]
     pass
 
 def grid_values(grid):
@@ -36,7 +51,11 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    board  = cross(alphas,digits)
+    values = dict(zip(board, grid))
+    for k,v in values.items():
+        if v == '.' : values[k] = digits
+    return values
 
 def display(values):
     """
@@ -44,10 +63,25 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
+    print()
+    spacer = '\t'
+    print(spacer.join([d for d in digits]))
+    for r in alphas:
+        print("")
+        row_str = []
+        row_str.append(r)
+        for d in digits:
+            row_str.append("{}".format(values["{}{}".format(r,d)]) )
+        print( spacer.join(row_str))
+    #print( values )
     pass
 
 def eliminate(values):
-    pass
+    for key,val in values.items():
+        if len(val) == 1:
+            for ccell in  cell_constraints[key]:
+                values[ccell] = values[ccell].replace(val, '')
+    return values
 
 def only_choice(values):
     pass
@@ -68,9 +102,61 @@ def solve(grid):
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
 
+    values = grid_values(grid)
+
+    display(values)
+
+    _boxes = [ list(map(''.join , itertools.product(A,B))) \
+               for A,B in list(itertools.product(['ABC','DEF','GHI'],['123','456','789'])) ]
+    boxes = { x:y for item in [{cell:box} for box in _boxes for cell in box ] for x, y in item.items() }
+
+    _diagonals = ["{}{}".format(x,y) for x, y in (zip(alphas, digits))]
+    diagonals = [ {cell:_diagonals} for cell in _diagonals ]
+    _diagonals = ["{}{}".format(x,y) for x, y in zip(reversed([a for a in alphas]), [d for d in digits]) ]
+    diagonals +=  [ {cell:_diagonals} for cell in _diagonals ]
+
+    diagonals = { x:y for item in diagonals for x, y in item.items() }
+
+    for cell in values:
+        cols[cell] = [ "{}{}".format(cell[0], i) for i in digits]
+        rows[cell] = [ "{}{}".format(a, cell[1]) for a in alphas]
+
+        if cell not in cell_constraints: cell_constraints[cell] = cols[cell].copy()
+
+        cell_constraints[cell] = cols[cell] + rows[cell] + boxes[cell]
+        if cell in diagonals:
+            cell_constraints[cell] += diagonals[cell]
+
+        cell_constraints[cell] = list(set(cell_constraints[cell]))
+        cell_constraints[cell].remove(cell)
+
+    print()
+    # print("---- boxes")
+    # print( boxes )
+    # print("---- cols")
+    # print( cols )
+    #
+    # print("-- rows")
+    # print( rows )
+    #
+    # print('----- diagonals')
+    # print( diagonals )
+    #
+    print('----cell constraints')
+    print( cell_constraints['A1'])
+
+    values = eliminate( values )
+
+    display ( values )
+
+    return
+    return values
+
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
+
+    sys.exit(0)
 
     try:
         from visualize import visualize_assignments
